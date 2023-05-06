5ff8b4e71091ec66c5014378b19373cd099ae9b2
Xazu <Xazu001@users.noreply.github.com>
2023-05-06 14:36:23 +0200

    Update1

---
+++ app.js
@@ -0,0 +1,83 @@
+const galleryMain = document.querySelector("#gallery-main");
+let showedCount = 4;
+
+const showMoreGallery = [
+    { src: 'bgs1.jpg', show: true },
+    { src: 'bgs2.jpg', show: true },
+    { src: 'bgs3.jpg', show: true },
+    { src: 'bgs4.jpg', show: true },
+    { src: 'bgs1.jpg', show: false },
+    { src: 'bgs2.jpg', show: false },
+    { src: 'bgs3.jpg', show: false },
+    { src: 'bgs4.jpg', show: false },
+    { src: 'bgs1.jpg', show: false },
+    { src: 'bgs2.jpg', show: false },
+    { src: 'bgs3.jpg', show: false },
+    { src: 'bgs4.jpg', show: false },
+    { src: 'bgs1.jpg', show: false },
+    { src: 'bgs2.jpg', show: false },
+    { src: 'bgs3.jpg', show: false },
+    { src: 'bgs4.jpg', show: false },
+    { src: 'bgs1.jpg', show: false },
+    { src: 'bgs2.jpg', show: false },
+    { src: 'bgs3.jpg', show: false },
+    { src: 'bgs4.jpg', show: false },
+    { src: 'bgs1.jpg', show: false },
+    { src: 'bgs2.jpg', show: false },
+    { src: 'bgs3.jpg', show: false },
+    { src: 'bgs4.jpg', show: false },
+];
+
+const showNext = () => {
+    for (i = showedCount; i < Math.min(showedCount + 5, showMoreGallery.length); i++) {
+        // showMoreGallery[i].show = true;
+        document.querySelector("#gallery-main :nth-child(" + i + ")").classList.add("active");
+    }
+    showedCount += 4;
+}
+
+const fix = () => {
+    for (i = 1; i <= 4; i++) {
+        document.querySelector("#gallery-main :nth-child(" + i + ")").style.setProperty("opacity", "1");
+    }
+}
+
+
+let elo = 4;
+let howMuchShowed = elo + 4;
+const imgTransition = () => {
+    howMuchShowed = howMuchShowed + 4;
+
+    setTimeout(() => {
+        for (a = elo, i = 1; a <= howMuchShowed, i <= 5; a++, i++) {
+            document.querySelector("#gallery-main :nth-child(" + a + ")").classList.add("d" + `${i}`);
+
+        }
+        elo = elo + 4;
+        i = 1;
+    }, 500);
+};
+
+const renderGallery = () => {
+    galleryMain.innerHTML = "";
+    showMoreGallery.forEach((galleryImg) => {
+        const newGalleryImg = `
+      <div class="gallery-img${galleryImg.show ? ' active' : ''}" >
+        <img src="${galleryImg.src}" alt="">
+      </div>
+    `;
+        galleryMain.innerHTML += newGalleryImg;
+    });
+
+}
+
+document.querySelector("button").addEventListener("click", () => {
+
+    showNext();
+    imgTransition();
+
+
+});
+
+renderGallery();
+fix();
